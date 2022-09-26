ac5ea5d3429ba46db5ae36052e4bd226b786411b
Brandon Santana <67774921+Undeadsyco@users.noreply.github.com>
2022-09-25 20:06:35 -0500

    Revert "added project v1"

--- v1/pos-system/src/actions/index.js
+++
@@ -1,4 +0,0 @@
-export { default as userActions } from './userActions';
-export { default as menuActions } from './menuActions';
-export { default as orderActions } from './orderActions';
-export { default as inventoryActions } from './inventoryActions';
